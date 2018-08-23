8fc544a2ff04e10acbbbe73d772a4f1203ed20fd
Davinport003 <42645218+Davinport003@users.noreply.github.com>
2018-08-23 16:33:50 +0300

    Add files via upload

---
+++ counter.py
@@ -0,0 +1,16 @@
+autor = "Davinport"
+ver = "0.1.2"
+name = "CounterI"
+
+i = 0
+def i():
+	i += 1
+
+def print():
+	print(i)
+
+def hello():
+	print("Hello, world!")
+
+def ver():
+	print(name,ver, "Made by ", autor)
